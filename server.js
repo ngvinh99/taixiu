425b5bd55353e794a937b1fb27413ff2bd67c5b3
ngvinh99 <vinhff724@gmail.com>
2025-07-01 10:15:46 +0700

    Update server.js

--- server.js
+++ server.js
@@ -515,7 +515,7 @@ async function broadcastPrediction() {
 
 
 function connectWebSocket() {
-  ws = new WebSocket("wss://websocket.azhkthg1.net/websocket");
+  ws = new WebSocket("wss://websocket.azhkthg1.net/websocket?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhbW91bnQiOjB9.p56b5g73I9wyoVu4db679bOvVeFJWVjGDg_ulBXyav8");
 
   ws.on("open", () => {
     console.log("Đã kết nối WebSocket thành công đến Sunwin.");
