4a9b2b954a886c1b8f1156480b876c9a6eecc8c0
ngvinh99 <vinhff724@gmail.com>
2025-07-02 16:32:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 fastify.register(fastifyWebsocket);
 
 // --- Cấu hình API Key và Auth cho WebSocket ---
-const API_KEY = "axosunvip"; // Thay đổi key này bằng key của bạn
+const API_KEY = "stvsunaxovip"; // Thay đổi key này bằng key của bạn
 
 // Middleware for HTTP API (still keeping it for now, can be removed)
 fastify.addHook("onRequest", async (request, reply) => {
